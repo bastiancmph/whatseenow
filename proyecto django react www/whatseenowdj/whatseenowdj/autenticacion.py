dab312463118b94850d7d537113b571cad86ab6f
bastiancmph <chavez.juan@correounivalle.edu.co>
2020-09-19 11:20:11 -0500

    inicio de desarrollo de autenticaciones 2

--- proyecto django react www/whatseenowdj/whatseenowdj/autenticacion.py
+++ proyecto django react www/whatseenowdj/whatseenowdj/autenticacion.py
@@ -6,12 +6,13 @@ from rest_framework import exceptions
 class Autenticacion(authentication.BaseAuthentication):
 
     def authenticate(self, request):
-        username = request.META.get('X_USERNAME')
-        if not username:
+        email = request.META.get('email')
+        password = request.META.get('password')
+        if ((not email) and (not password)):
             return None
 
         try:
-            Usuario = Usuario.objects.get(Usuarioname=username)
+            Usuario = Usuario.objects.get(email=email, password=password)
         except exceptions.NotFound:
             raise exceptions.AuthenticationFailed('No such user')
 
